34d6499e160e1bf88a33518ddccfb57ef3045f11
Andres Guerrero <aguerrero@fi.uba.ar>
2024-07-17 10:13:25 -0300

    add useCounter test

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -7,12 +7,12 @@ export const useCounter = ( initialValue = 10 ) => {
     const [counter, setcounter] = useState( initialValue );
 
     const increment = ( value = 1 ) => {
-        setcounter( counter + value );
+        setcounter( (current) => current + value );
     }
 
     const decrement = ( value = 1 ) => {
         if ( counter <= 0 ) return ;
-        setcounter( counter - value );
+        setcounter( (current) => current - value );
     }
 
     const reset = () => {
