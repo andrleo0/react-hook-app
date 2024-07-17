3cc0a6d787b9a497765d247d9f95f2bf2dc49334
Andres Guerrero <aguerrero@fi.uba.ar>
2024-07-17 16:23:41 -0300

    feat: create jest-setup

---
+++ test/03-examples/MultipleCustomHooks.test.js
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react"
+import { MultipleCustomHooks } from "../../src/03-examples";
+
+describe('Pruebas en el <MultipleCustomHooks />', () => { 
+    test('debe de mostrar el componente por defecto', () => { 
+
+        render( <MultipleCustomHooks />);
+
+        
+    })  
+})
