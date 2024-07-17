85e72d9a269a7233a8cbea02c228102fb2cac12b
Andres Guerrero <aguerrero@fi.uba.ar>
2024-07-17 19:15:34 -0300

    add MultiplesCustomHooks-test

--- test/03-examples/MultipleCustomHooks.test.js
+++ test/03-examples/MultipleCustomHooks.test.js
@@ -1,11 +1,87 @@
-import { render } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { MultipleCustomHooks } from "../../src/03-examples";
+import { useFetch } from "../../src/hooks/useFetch";
+import { useCounter } from "../../src/hooks/useCounter";
 
-describe('Pruebas en el <MultipleCustomHooks />', () => { 
+jest.mock('../../src/hooks/useFetch');
+jest.mock('../../src/hooks/useCounter');
+
+const bulbasaurData =  {
+    name:'bulbasaur',
+    sprites:{
+        front_default:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+        front_shiny:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png',
+        back_default:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png',
+        back_shiny:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png'
+    }         
+}
+
+describe('Pruebas en el <MultipleCustomHooks />', () => {
+
+    const mockCounter = 1;
+    const mockIncrement = jest.fn();
+    const mockDecrement = jest.fn();
+
+    useCounter.mockReturnValue({
+        counter: mockCounter,
+        decrement: mockDecrement,
+        increment: mockIncrement
+    });
+
+    beforeEach( () => {
+        jest.clearAllMocks();
+    });
+
     test('debe de mostrar el componente por defecto', () => { 
+
+        useFetch.mockReturnValue({
+            data: null,
+            isLoading: true,
+            hasError: null,
+        });
 
         render( <MultipleCustomHooks />);
 
-        
-    })  
-})
+        expect( screen.getByText('Loading...'));
+        expect( screen.queryByText('Informacion de Pokemon'));
+
+        const buttonSiguiente = screen.getByRole('button',{ name: 'Siguiente'});
+        expect( buttonSiguiente.disabled ).toBeFalsy();
+
+    });
+    
+    test('debe de mostrar el nombre e imagenes del pokemon', () => {
+
+        useFetch.mockReturnValue({
+            data: bulbasaurData,
+            isLoading: false,
+            hasError: null,
+        });
+
+        render( <MultipleCustomHooks />);
+
+        expect( screen.getByText(`#${mockCounter} - ${bulbasaurData.name}`) );
+        const spriteImages = screen.getAllByRole('img');
+        spriteImages.forEach( (img, index) => {
+            expect( img.src ).toBe( bulbasaurData.sprites[Object.keys(bulbasaurData.sprites)[index]]);
+        });
+
+    });
+
+    test('debe de llamar la funcion de incrementar', () => {    
+    
+        useFetch.mockReturnValue({
+            data: bulbasaurData,
+            isLoading: false,
+            hasError: null,
+        });
+
+        render( <MultipleCustomHooks />);
+
+        const buttonSiguiente = screen.getByRole('button',{ name: 'Siguiente'});
+        fireEvent.click( buttonSiguiente );
+
+        expect( mockIncrement ).toHaveBeenCalled();
+
+    });
+});
